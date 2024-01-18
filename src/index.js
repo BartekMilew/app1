2f8123ce9806a5b76c3514679b2d7964b234aaf8
BartekMilew <bartosz.milew@gmail.com>
2024-01-18 21:43:57 +0100

    ble

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <p>App1</p>
+    <iframe
+      name="app2"
+      src="https://app2-bartoszs-projects-6ee0ba76.vercel.app/"
+      width="100% "
+    />
   </React.StrictMode>
 );
 
