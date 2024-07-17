582774f64e719a949d32fa3269e9b0df86300580
BartekMilew <bartosz.milew@gmail.com>
2024-07-17 11:41:56 +0200

    new change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Passkeys from "./passkeys/Passkeys";
+import Device from "./passkeys/Device";
 
 function App() {
   return (
     <div className="App">
       <p>App1</p>
       <Passkeys />
+      <Device />
     </div>
   );
 }
---
+++ src/passkeys/Device.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { UAParser } from "ua-parser-js";
+
+const Device = () => {
+  const uaParser1 = new UAParser();
+  const uaParser1Results = uaParser1.getResult();
+  console.log(uaParser1Results);
+  const uaParser2Results = uaParser1.getDevice().withFeatureCheck();
+  console.log(uaParser2Results, "2results");
+  let isIPad =
+    /Macintosh/i.test(navigator.userAgent) &&
+    navigator.maxTouchPoints &&
+    navigator.maxTouchPoints > 1;
+
+  console.log(isIPad);
+  return (
+    <>
+      <div>uaParser1</div>
+      <div>{JSON.stringify(uaParser1Results)}</div>
+      <div>uaParser2</div>
+      <div>{JSON.stringify(uaParser2Results)}</div>
+      <div>uaParser3</div>
+      <div>is Ipad{isIPad ? "true" : "false"}</div>
+    </>
+  );
+};
+
+export default Device;
